a25511f986175e95b9c42a548f1320aa4b0fe8b0
kairat123123 <147539620+kairat123123@users.noreply.github.com>
2023-11-17 18:11:55 +0600

    1 homework-react
    1 homework-react

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+
+
+
+export default function MyApp() {
+    const [count, setCount] = useState(0);
+
+    function  handleClick() {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <h1>sydgaihfasuufha</h1>
+            <MyButton count={count} onClick={handleClick} />
+            <MyButton count={count} onClick={handleClick} />
+        </div>
+    );
+}
+
+function  MyButton ({ count, onClick }) {
+    return <button onClick={onClick}>Clicked {count} times!</button>
+}
